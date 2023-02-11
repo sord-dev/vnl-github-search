d53ab2e9927cdaa5273f534503b6d4a19511d142
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-02-11 09:02:17 +0000

    add vite config and deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:sord-dev/vnl-github-search.git main:gh-pages
+
+cd -
